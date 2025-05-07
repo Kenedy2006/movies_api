88a939e9862898d9d38748177aca20d15aa0d28e
Kenedy2006 <jeyson.ramos@tecsup.edu.pe>
2025-05-07 16:40:55 -0500

    Primer commit

--- movies/serializers.py
+++ movies/serializers.py
@@ -18,12 +18,28 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class MovieSerializer(serializers.ModelSerializer):
     director = DirectorSerializer()
-    actors = ActorSerializer(many=True)
-    genres = GenreSerializer(many=True)
+    actors = serializers.PrimaryKeyRelatedField(queryset=Actor.objects.all(), many=True)  # Acepta múltiples actores
+    genres = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), many=True)  # Acepta múltiples géneros
 
     class Meta:
         model = Movie
         fields = ['id', 'title', 'description', 'release_date', 'director', 'actors', 'genres']
+
+    def create(self, validated_data):
+        director_data = validated_data.pop('director')  # Extraer el director
+        director = Director.objects.create(**director_data)  # Crear el director
+
+        actors_data = validated_data.pop('actors')  # Extraer actores
+        genres_data = validated_data.pop('genres')  # Extraer géneros
+
+        movie = Movie.objects.create(director=director, **validated_data)  # Crear la película
+
+        # Asociar actores y géneros a la película
+        movie.actors.set(actors_data)
+        movie.genres.set(genres_data)
+
+        return movie
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
